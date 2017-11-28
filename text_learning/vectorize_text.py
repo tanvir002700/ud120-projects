683e954116fba885499eb322aad55e40e2c3f9b1
tanvir hasan <tanvir002700@gmail.com>
2017-11-29 01:58:51 +0600

    vectorize email data

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -42,20 +42,28 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
         temp_counter += 1
-        if temp_counter < 200:
+        if True:
             path = os.path.join('..', path[:-1])
             print(path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
 
+            words = parseOutText(email)
+
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            for x in ["sara", "shackleton", "chris", "germani"]:
+                words.replace(x, '')
+
 
             ### append the text to word_data
 
+            word_data.append(words)
+
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
+            from_data.append(name == 'sara')
 
             email.close()
 
