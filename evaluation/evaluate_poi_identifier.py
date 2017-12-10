6ccc6f866613f813213429489e68672c6e89ed42
tanvir hasan <tanvir002700@gmail.com>
2017-12-11 01:54:12 +0600

    solve evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,17 +15,28 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson14_keys.pkl')
+data = featureFormat(data_dict, features_list, sort_keys='../tools/python2_lesson14_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
+features_train,features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
+
+pred = clf.predict(features_test)
+print(sum(pred))
+print(len(pred))
+
 
 
