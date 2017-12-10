1fbbbe611d8a0a4dda1f9c9f2d8f7f63b357e644
tanvir hasan <tanvir002700@gmail.com>
2017-12-10 22:58:36 +0600

    validation solution

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,8 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
@@ -28,5 +30,16 @@ labels, features = targetFeatureSplit(data)
 
 
 ### it's all yours from here forward!
+
+clf = DecisionTreeClassifier()
+clf.fit(features, labels)
+print(clf.score(features, labels))
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3,
+                                                                            random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+print(clf.score(features_test, labels_test))
 
 
